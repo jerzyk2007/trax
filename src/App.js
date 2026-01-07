4a34841fd8bd788b46db3315940487a1da233be7
jerzyk2007 <jerzykomorowski77@gmail.com>
2026-01-07 17:39:07 +0100

    przygotowanie do deploy na git

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+// import React from "react";
+// import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+// import Home from "./components/Home";
+// import Navbar from "./components/menu/Navbar";
+// import UsedCars from "./components/UsedCars";
+// import Locations from "./components/Locations";
+// import CookieBanner from "./components/CookieBanner";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <BrowserRouter > */}
+//       <HashRouter>
+//         <Navbar />
+//         {/* <Home /> */}
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/osobowe" element={<UsedCars />} />
+//           <Route path="/locations" element={<Locations />} />
+//         </Routes>
+//         <CookieBanner />
+//       </HashRouter>
+//       {/* </BrowserRouter> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/menu/Navbar";
 import UsedCars from "./components/UsedCars";
@@ -10,18 +40,29 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {/* <BrowserRouter > */}
+      {/* HashRouter jest idealny dla GitHub Pages, ponieważ używa znaku # w adresie URL, 
+          co zapobiega błędom 404 przy odświeżaniu strony. */}
       <HashRouter>
+        {/* Navbar musi być wewnątrz Routera, aby Linki działały poprawnie */}
         <Navbar />
-        {/* <Home /> */}
+
         <Routes>
+          {/* Strona główna */}
           <Route path="/" element={<Home />} />
+
+          {/* Trasa dla samochodów osobowych (używane) */}
           <Route path="/osobowe" element={<UsedCars />} />
+
+          {/* Trasa dla lokalizacji serwisu */}
           <Route path="/locations" element={<Locations />} />
+
+          {/* Opcjonalnie: Przekierowanie dla nieistniejących ścieżek z powrotem do Home */}
+          <Route path="*" element={<Home />} />
         </Routes>
+
+        {/* Baner cookies widoczny na każdej podstronie */}
         <CookieBanner />
       </HashRouter>
-      {/* </BrowserRouter> */}
     </div>
   );
 }
